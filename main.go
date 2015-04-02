51aacb28ebeba2719c71e4c04329892c3a5bbb8e
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-04-03 01:43:45 +0200

    30917

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/prestonTao/upnp"
+
 	"log"
   "./tools/"
+	"./upnp/"
 	"os"
 )
 
@@ -13,8 +14,7 @@ import (
 func init() {
 
 	if (os.Getuid() == 0) || (os.Getgid() == 0) {
-		fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
-		fmt.Println("This is not a tree! We need no roots!")
+		log.Printf("[OMG] %s", "AAAARGH! ROOT! ROOT! ROOOOOT! ")
 		os.Exit(1)
 	}
 
@@ -25,17 +25,7 @@ func main() {
 	tools.ReadIpFromInterface()
 	fmt.Println("now by host")
 	tools.ReadIpFromHost()
-	NntpUpnpOpen()
+	upnp.AllUpnpOpen()
 	os.Exit(0)
 
-}
-
-func NntpUpnpOpen() {
-
-	mapping := new(upnp.Upnp)
-	if err := mapping.AddPortMapping(11119, 11119, "TCP"); err == nil {
-		log.Printf("[INFO] %s", "UPnP redirect 11119 successful")
-	} else {
-		log.Printf("[WARNING] %s", "UPnP redirect failed for port 11119: router is UPnP-agnostic")
-	}
 }
---
+++ upnp/upnp.go
@@ -0,0 +1,28 @@
+package upnp
+
+import (
+	"log"
+  "github.com/prestonTao/upnp"
+)
+
+func AllUpnpOpen() {
+
+	mapping_nntp := new(upnp.Upnp)
+	if err := mapping_nntp.AddPortMapping(11119, 11119, "TCP"); err == nil {
+		log.Printf("[INFO] %s", "UPnP redirect 11119 successful")
+	} else {
+		log.Printf("[WARNING] %s", "UPnP redirect failed for port 11119: router is UPnP-agnostic")
+	}
+
+  mapping_dht := new(upnp.Upnp)
+  if err := mapping_dht.AddPortMapping(20000, 20000, "TCP"); err == nil {
+    log.Printf("[INFO] %s", "UPnP redirect 20000 successful")
+  } else {
+    log.Printf("[WARNING] %s", "UPnP redirect failed for port 20000: router is UPnP-agnostic")
+  }
+
+
+
+
+
+}
