6f1ff9926147dbdb12048d875b54986756187551
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-04-02 15:09:27 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package averno
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 
 func init() {
-    if (os.Getuid() == 0) || ((os.Getgid() == 0) {
-    fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
-    fmt.Println("This is not a tree! We need no root!")
-    os.Exit(1)
+	
+		if (os.Getuid() == 0) || ((os.Getgid() == 0) {
+    		fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
+    		fmt.Println("This is not a tree! We need no root!")
+    		os.Exit(1)
                         }
+	
 }
 
 
 func main() {
 	
-	
+	os.exit(0)	
 	
 }
