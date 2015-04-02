e663630e17b0b3e50f8078f4e5d386d256f31498
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-04-02 15:26:09 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func init() {
 	
 		if (os.Getuid() == 0) || ((os.Getgid() == 0) {
     		fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
-    		fmt.Println("This is not a tree! We need no root!")
+    		fmt.Println("This is not a tree! We need no roots!")
     		os.Exit(1)
                         }
 	
