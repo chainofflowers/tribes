93af89fa8f803c7c80c4458111944675199d3330
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-04-02 20:10:07 +0200

    31336

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func init() {
 	
-		if (os.Getuid() == 0) || ((os.Getgid() == 0) {
+		if (os.Getuid() == 0) || (os.Getgid() == 0) {
     		fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
     		fmt.Println("This is not a tree! We need no roots!")
     		os.Exit(1)
@@ -20,6 +20,6 @@ func init() {
 
 func main() {
 	
-	os.exit(0)	
+	os.Exit(0)	
 	
 }
