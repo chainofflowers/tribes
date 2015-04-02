ab8f89bc01d59ecd2c48b47bf009103c712d2587
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-04-03 00:46:27 +0200

    16249

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/prestonTao/upnp"
 	"log"
+	"net"
+
 	"os"
 )
 
-// just to avoid running it as root
+// No root. End of story
 
 func init() {
 
@@ -19,15 +21,22 @@ func init() {
 
 }
 
+const (
+	httpPortTCP = 8080
+	numTarget   = 10
+	exampleIH   = "deca7a89a1dbdc4b213de1c0d5351e92582f31fb" // ubuntu-12.04.4-desktop-amd64.iso
+)
+
 func main() {
 
-	NNTP_UPnP_open()
+	ReadIpFromInterface()
+	NntpUpnpOpen()
 
 	os.Exit(0)
 
 }
 
-func NNTP_UPnP_open() {
+func NntpUpnpOpen() {
 
 	mapping := new(upnp.Upnp)
 	if err := mapping.AddPortMapping(11119, 11119, "TCP"); err == nil {
@@ -35,4 +44,16 @@ func NNTP_UPnP_open() {
 	} else {
 		log.Printf("[WARNING] %s", "UPnP redirect failed for port 11119: router is UPnP-agnostic")
 	}
+}
+
+func ReadIpFromInterface() {
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+
+			log.Printf("[INFO] %s", ipv4)
+		}
+	}
+
 }
