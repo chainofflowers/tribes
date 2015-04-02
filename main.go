0155d8056d74d47493da790f77873db779914f9a
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-04-02 20:23:05 +0200

    7819

--- main.go
+++ main.go
@@ -8,18 +8,17 @@ import (
 // just to avoid running it as root
 
 func init() {
-	
-		if (os.Getuid() == 0) || (os.Getgid() == 0) {
-    		fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
-    		fmt.Println("This is not a tree! We need no roots!")
-    		os.Exit(1)
-                        }
-	
+
+	if (os.Getuid() == 0) || (os.Getgid() == 0) {
+		fmt.Println("AAAARGH! ROOT! ROOT! ROOOOOT! ")
+		fmt.Println("This is not a tree! We need no roots!")
+		os.Exit(1)
+	}
+
 }
 
-
 func main() {
-	
-	os.Exit(0)	
-	
+
+	os.Exit(0)
+
 }
